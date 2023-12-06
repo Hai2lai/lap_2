07153d3ea70a190b25d640b81cc77411caaa69dc
Hai2lai <152401246+Hai2lai@users.noreply.github.com>
2023-12-06 21:33:17 +0700

    tinh_thoi_gian_hien_tai

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DateTime locaDate = DateTime.Now;
+        System.Console.WriteLine("datetime now is : " + locaDate);
+}
+}
